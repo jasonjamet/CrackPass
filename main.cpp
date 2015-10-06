4d2f0c4ce3a792923fe6097c847fc5d5f1c9fb6a
Mattieu <mattieu_racine@hotmail.fr>
2015-10-06 16:15:32 +0200

    Create Project

---
+++ main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by etudiant on 06/10/15.
+//
+
+#include "main.h"
+
+int main() {
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by etudiant on 06/10/15.
+//
+
+#ifndef CRACKPASS_MAIN_H
+#define CRACKPASS_MAIN_H
+
+#endif //CRACKPASS_MAIN_H
