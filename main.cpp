b12f5472c8e403059335cd421f1109ea4ba18582
Mattieu <mattieu_racine@hotmail.fr>
2015-10-07 12:18:27 +0200

    Add Function BrutForce

--- main.cpp
+++ main.cpp
@@ -2,10 +2,19 @@
 // Created by etudiant on 06/10/15.
 //
 
-#include "main.h"
+#include "Functions.h"
+
+
+char *mdp = "aaaa";
 
 
 int main() {
+
+    Functions *F = new Functions("salt", mdp);
+
+    F->bruteForce(4);
+
+    delete (F);
 
     return 0;
 }
