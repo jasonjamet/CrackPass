3b0ff103822624200c0cad39c10548b0fd0bace9
jasonjamet <jason.jamet@gmail.com>
2015-10-10 14:23:13 +0200

    merge soft with qt

--- main.cpp
+++ main.cpp
@@ -3,22 +3,19 @@
 //
 
 #include "Functions.h"
-#include "QT/Crack/mainwindow.h"
-#include <QApplication>
+//#include "QT/Crack/mainwindow.h"
+//#include <QApplication>
 
 
 
-    int main(int argc, char *argv[])
-    {
-        QApplication a(argc, argv);
-        MainWindow w;
-        w.show();
+int main(int argc, char *argv[]) {
+    //QApplication a(argc, argv);
+    //MainWindow w;
+    //w.show();
 
-        return a.exec();
-    }
-
-    //Functions *F = new Functions();
-    //F->lauchSimpleBruteForce(F->getPasswordEncryptedByName(F->readShadowFile("shadow"), "jason"), 4);
-    //delete (F);
-
-    //return 0;
+    //return a.exec();
+    Functions *F = new Functions();
+    F->lauchSimpleBruteForce(F->getPasswordEncryptedByName(F->readShadowFile("shadow"), "jason"), 4);
+    delete (F);
+    return 0;
+}
