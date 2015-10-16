fa64d68fa9f8361c410e1127c3174534c069c2d8
jasonjamet <jason.jamet@gmail.com>
2015-10-16 14:07:39 +0200

    add option exec

--- main.cpp
+++ main.cpp
@@ -8,45 +8,45 @@ using namespace std;
 int main(int argc, char *argv[]) {
 
     if(argc >1) {
-        Functions * F = new Functions();
+        if(atoi(argv[1]) == 0) {
+            Functions * F = new Functions();
 
+            int rank;
+            MPI_Init(&argc, &argv);
 
+            MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Rank
+            char * message = (char *) malloc(100);
 
-        int rank;
-        MPI_Init(&argc, &argv);
-
-        MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Rank
-        char * message = (char *) malloc(100);
-
-        if(rank == 0) {
-            map<string, string> userAndPass = F->readShadowFile("shadow");
-            F->setPasswordEncryptedByName(userAndPass, argv[1]);
-            strcpy(message, F->getHash().c_str());
-        }
-        MPI_Bcast(message, 100, MPI_CHAR, 0, MPI_COMM_WORLD);
-
-        if (rank==0) {
-
-            //MPI_Send(message, 100, MPI_CHAR, MPI_, 7, MPI_COMM_WORLD);
-
-            MPI_Recv(message, 100, MPI_CHAR, MPI_ANY_SOURCE, 7, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            cout << "Password found: " << message << endl;
-            MPI_Abort(MPI_COMM_WORLD, MPI_SUCCESS);
-
-        }
-        else {
-
-            F->setHash(message);
-            cout << F->getHash() << endl;
-            if(atoi(argv[2]) == 1) {
-                F->launchSimpleBruteForce();
-            } else {
-                F->launchDictionaryBruteForce();
+            if(rank == 0) {
+                map<string, string> userAndPass = F->readShadowFile("shadow");
+                F->setPasswordEncryptedByName(userAndPass, argv[2]);
+                strcpy(message, F->getHash().c_str());
             }
+            MPI_Bcast(message, 100, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+            if (rank==0) {
+                MPI_Recv(message, 100, MPI_CHAR, MPI_ANY_SOURCE, 7, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                cout << "Password found: " << message << endl;
+                MPI_Abort(MPI_COMM_WORLD, MPI_SUCCESS);
+
+            }
+            else {
+
+                F->setHash(message);
+                cout << F->getHash() << endl;
+                if(atoi(argv[3]) == 1) {
+                    F->launchSimpleBruteForce();
+                } else {
+                    F->launchDictionaryBruteForce();
+                }
+            }
+
+            MPI_Barrier(MPI_COMM_WORLD);
+            MPI_Finalize();
+        } else if(atoi(argv[1]) == 1) {
+            
         }
 
-        MPI_Barrier(MPI_COMM_WORLD);
-        MPI_Finalize();
 
     } else {
         cout << "arguments are missing" << endl;
